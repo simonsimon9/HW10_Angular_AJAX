63701f6330688f878540f6dbc88a3c4707aef4c6
simonsimon9 <simonkuang9@outlook.com>
2022-06-08 02:22:24 -0400

    Async call for remote resource

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<button (click)="getSomeData()">Test GET Request</button>
+  <ul>
+  <li *ngFor="let myData of namesArray">{{myData.first}}
+  {{myData.last}}</li>
+  </ul>`
 })
 export class AppComponent {
-  title = 'angular_ajax';
+   namesArray!: Array<any>;
+  _http:HttpClient;
+
+  //since we are using a provider above we can recieve an 
+  //instance through an instructor
+
+  constructor(private http: HttpClient){
+    this._http = http;
+  }
+
+  getSomeData(){
+    this._http.get<any>('./assets/test.json')
+      .subscribe(result=>{
+        this.namesArray =result;
+      },
+      error=>{
+        alert(error);
+        console.log(error);
+      }
+      )
+  }
 }
