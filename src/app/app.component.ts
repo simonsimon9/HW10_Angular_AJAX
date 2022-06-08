127c7fb3b1caef3a2e3a205a8c19789582e3c5df
simonsimon9 <simonkuang9@outlook.com>
2022-06-08 17:32:12 -0400

    Events: Mouse enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,14 @@
 import { Component } from '@angular/core';
-// This component consumes the re-usable service.
 @Component({
 selector: 'app-root',
-template: `
-<h3>Please enter the customer informatino:</h3>
-  <child [callParent]="parentFuncRef"  [city]="op"></child>
-{{dataFromChild}}
-`
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    op ='detroit'
-    parentFuncRef!: Function;
-    operations!: Array<any>;
-    dataFromChild!: string;
-// This function is called by the Angular framework after
-// the constructor executes.
-public ngOnInit() {
-    this.parentFuncRef = this.myCallBackFunction.bind(this);
-}
-// This function can be called by child.
-public myCallBackFunction(streetAddress:any, city:any, region: any) {
-  this.dataFromChild =
-  "Street Address: " + streetAddress + " " +
-  "City: " + city + " " +
-  "Region: " + region;
+  title = 'app';
+  mouseLog = "";
+  myMouseEnter() {
+  console.log("Mouse entered!")
+  this.mouseLog+= "entered!"
 }
 }
